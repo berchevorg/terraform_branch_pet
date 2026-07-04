5d629ffa5203038a1af89d4a7139c9b319bd5fdc
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2026-07-04 11:39:35 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "random_pet" "name1" {
+ length    = "7"
+ separator = "-"
+}
+
+#resource "random_pet" "name2" {
+ #length    = "5"
+ #separator = "-"
+#}
+
+#resource "random_pet" "name3" {
+ #length    = "44"
+ #separator = "-"
+#}
